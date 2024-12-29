f5b5058ed5683af50cd22580274154237d470131
Irhamms <birhamshidiq@gmail.com>
2024-12-29 14:25:26 +0700

    feat: upd front-end home

--- app/home/api.py
+++ app/home/api.py
@@ -1,17 +1,17 @@
 from flask import jsonify, request, json, session, redirect
 
-from app.api.db_service import execute_query
+# from app.api.db_service import execute_query
 from . import home
 
 
 @home.route('/api/home/getTest', methods=['GET'])
 def getTest():
     try:
-        select_country = """
-            SELECT * from table;
-            """
-        country_result = execute_query(select_country, 'db1')
-        return jsonify({"status": "ok", "output": country_result}), 200
+        # select_country = """
+        #     SELECT * from table;
+        #     """
+        # country_result = execute_query(select_country, 'db1')
+        return jsonify({"status": "ok", "output": "HOME"}), 200
         
     except Exception as err:
         return jsonify({"status": "failed", "message": str(err)}), 200
--- app/list/api.py
+++ app/list/api.py
@@ -1,17 +1,17 @@
 from flask import jsonify, request, json, session, redirect
 
-from app.api.db_service import execute_query
+# from app.api.db_service import execute_query
 from . import list
 
 
 @list.route('/api/list/getTest', methods=['GET'])
 def getTest():
     try:
-        select_country = """
-            SELECT * from table;
-            """
-        country_result = execute_query(select_country, 'db1')
-        return jsonify({"status": "ok", "output": country_result}), 200
+        # select_country = """
+        #     SELECT * from table;
+        #     """
+        # country_result = execute_query(select_country, 'db1')
+        return jsonify({"status": "ok", "output": "LIST"}), 200
         
     except Exception as err:
         return jsonify({"status": "failed", "message": str(err)}), 200
